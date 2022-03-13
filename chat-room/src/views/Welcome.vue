fa03ff96da695f6471886f365dc50ec6d8fb620c
aelmore49 <alexelmore49@gmail.com>
2022-03-13 09:34:30 -0500

    More project setup

---
+++ chat-room/src/views/Welcome.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="welcome container">Welcome</div>
+</template>
+
+<script>
+export default {
+	name: "Welcome",
+};
+</script>
+
+<style>
+.welcome {
+	text-align: center;
+	padding: 20px 0;
+}
+/* form styles */
+.welcome form {
+	width: 300px;
+	margin: 20px auto;
+}
+.welcome label {
+	display: block;
+	margin: 20px 0 10px;
+}
+.welcome input {
+	width: 100%;
+	padding: 10px;
+	border-radius: 20px;
+	border: 1px solid #eee;
+	outline: none;
+	color: #999;
+	margin: 10px auto;
+}
+.welcome span {
+	font-weight: bold;
+	text-decoration: underline;
+	cursor: pointer;
+}
+.welcome button {
+	margin: 20px auto;
+}
+</style>
