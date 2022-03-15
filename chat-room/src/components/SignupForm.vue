9b36f87008221b9a7ffa0c8fd93f6f64a8d199dd
Alex Elmore <aelmore@mrk-c02gf1f1md6r.lan>
2022-03-15 08:45:16 -0500

    Setup for useSignup composable

--- chat-room/src/components/SignupForm.vue
+++ chat-room/src/components/SignupForm.vue
@@ -1,38 +1,44 @@
 <template>
-	<form @submit.prevent="handleSubmit">
-		<label> User Name:</label>
-		<input type="text" required placeholder="User Name" v-model="displayName" />
+  <form @submit.prevent="handleSubmit">
+    <label> User Name:</label>
+    <input type="text" required placeholder="User Name" v-model="displayName" />
 
-		<label> Email:</label>
-		<input type="email" required placeholder="Email" v-model="email" />
+    <label> Email:</label>
+    <input type="email" required placeholder="Email" v-model="email" />
 
-		<label> Password:</label>
-		<input type="password" required placeholder="Password" v-model="password" />
+    <label> Password:</label>
+    <input type="password" required placeholder="Password" v-model="password" />
 
-		<button>Sign Up</button>
-	</form>
+    <button>Sign Up</button>
+  </form>
 </template>
 
 <script>
 import { ref } from "vue";
+import useSignup from "../composables/useSignup";
+import { useRouter } from "vue-router";
 export default {
-	name: "SignupForm",
-	setup(props) {
-		// Refs binded to form input fields
-		const displayName = ref("");
-		const email = ref("");
-		const password = ref("");
+  name: "SignupForm",
+  setup() {
+    // Pull out error and signup function from useSignup
+    const { error, signup } = useSignup();
 
-		// Function to handle form submissions
-		const handleSubmit = () => {
-			console.log({
-				DN: displayName.value,
-				Email: email.value,
-				PS: password.value,
-			});
-		};
-		return { displayName, email, password, handleSubmit };
-	},
+    // Init router for redirecting user to the home page after a successful signup
+    const router = useRouter();
+
+    // Refs binded to form input fields
+    const displayName = ref("");
+    const email = ref("");
+    const password = ref("");
+
+    // Function to handle form submissions
+    const handleSubmit = async () => {
+      await signup(email.value, password.value, displayName.value);
+      // Redirect user to home page after successful signup
+      router.push("/");
+    };
+    return { displayName, email, password, handleSubmit };
+  },
 };
 </script>
 
